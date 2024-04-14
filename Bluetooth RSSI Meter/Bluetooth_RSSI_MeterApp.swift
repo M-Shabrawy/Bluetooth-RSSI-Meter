51a1890f38bbb0ba1d5b66d328242e93885e2bcc
Mohamed AlShabrawy <m.s.elshabrawy@gmail.com>
2024-04-14 20:53:15 +0300

    Initial Commit

---
+++ Bluetooth RSSI Meter/Bluetooth_RSSI_MeterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bluetooth_RSSI_MeterApp.swift
+//  Bluetooth RSSI Meter
+//
+//  Created by Mohamed Alshabrawy on 14/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct Bluetooth_RSSI_MeterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
